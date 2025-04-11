f87423a1756d7efb8279e42e74fca77965c01449
scotth294 <scott.colfer@herd.consulting>
2025-04-11 17:56:31 +0100

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Optional – you can remove if you're not using it
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
